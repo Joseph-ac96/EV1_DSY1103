5f3c105c1474cf5d0dacd37fc5f700cdde741083
JoseAC <jos.amdor@duocuc.cl>
2026-03-25 22:54:21 -0300

    Proyecto iniciado

---
+++ src/main/java/com/tienda/ev1_dsy1103/Ev1Dsy1103Application.java
@@ -0,0 +1,13 @@
+package com.tienda.ev1_dsy1103;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ev1Dsy1103Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ev1Dsy1103Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tienda/ev1_dsy1103/Ev1Dsy1103ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.ev1_dsy1103;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ev1Dsy1103ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
